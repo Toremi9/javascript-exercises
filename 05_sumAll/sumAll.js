7fa37c4410ae05a2d4364f8a6aa35ea33e6b8274
Toremi9 <142730474+Toremi9@users.noreply.github.com>
2023-10-13 19:43:18 +0200

    completed 4-6 js excercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(numone, numtwo) {
+    let result = 0;
+    if (typeof numone !== "number"|| typeof numtwo !== "number") {
+        return "ERROR";
+    }
+    if (numone < numtwo) {
+        for (let i = numone; i <= numtwo; i++)
+        result += i;
+    } else {
+        for (let i = numtwo; i <= numone; i++)
+        result += i;
+    }
+    if (result < 0 || numone == NaN || numtwo == NaN) {
+        return "ERROR";
+    }
+    return result;
 };
 
 // Do not edit below this line
